5671f862a977f37474d73880b1d1e5ef7d575e3b
alejandro <alejandro@rithm.nl>
2024-10-08 13:41:25 -0300

    Messages form finished

--- src/main/java/dev/alvartaco/notifications/controller/MessageController.java
+++ src/main/java/dev/alvartaco/notifications/controller/MessageController.java
@@ -2,21 +2,22 @@ package dev.alvartaco.notifications.controller;
 
 import dev.alvartaco.notifications.dto.CategoryDTO;
 import dev.alvartaco.notifications.exception.CategoryNotFoundException;
-import dev.alvartaco.notifications.repository.CategoryRepository;
 import dev.alvartaco.notifications.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class MessageController {
+public class  MessageController {
 
+    static final String MESSAGE = "message";
+    static final String ERROR = "error";
+
+    private static final Logger log = LoggerFactory.getLogger(MessageController.class);
     private final CategoryService categoryService;
 
     public MessageController( CategoryService categoryService) {
@@ -24,16 +25,47 @@ public class MessageController {
     }
 
     @GetMapping("/message")
-    public String message(Model model) {
-        List<CategoryDTO> categories = null;
+    public String message(@RequestParam(name = "error", defaultValue = "") String error,
+                          @RequestParam(name = "message", defaultValue = "") String message,
+                          Model model) {
+
+        if (!message.isEmpty())
+            model.addAttribute(MESSAGE, message);
+        if (!error.isEmpty())
+            model.addAttribute(ERROR, error);
+
+        List<CategoryDTO> categories;
         try {
             categories = categoryService.getAllByCategoryNameAsc();
         } catch (CategoryNotFoundException e) {
-            throw new RuntimeException(e);
+            // TESTED //
+            log.error("#NOTIFICATIONS - Error getting categories /message, fwd to index.");
+            return "index";
         }
         model.addAttribute("categorySelect", categories);
         return "message/index";
     }
 
+    @PostMapping("/message/create")
+    String createMessage(@RequestParam(name = "categoryId") String categoryId,
+                         @RequestParam(name = "messageBody") String messageBody,
+                         Model model) {
+
+        List<CategoryDTO> categories;
+        try {
+            categories = categoryService.getAllByCategoryNameAsc();
+        } catch (CategoryNotFoundException e) {
+            // TESTED //
+            log.error("#NOTIFICATIONS - Error getting categories /message/create, fwd to index.");
+            return "index";
+        }
+
+        if (categories.stream().noneMatch(dto -> dto.getCategoryId() == Short.parseShort(categoryId))) {
+            // TESTED /
+            log.error("#NOTIFICATIONS - Error with received categoryID /message/create");
+            return message("ERROR with received Message Category!!!", "", model);
+        }
+        return message("", "Message Saved..!", model);
+    }
 }
 
