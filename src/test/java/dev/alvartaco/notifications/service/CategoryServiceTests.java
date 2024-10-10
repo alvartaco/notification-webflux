bd7a723ffb0cdbbfc15c08faba64f7e1d55b83c3
alejandro <alejandro@rithm.nl>
2024-10-10 20:12:02 -0300

    Adding Test Classes

--- src/test/java/dev/alvartaco/notifications/service/CategoryServiceTests.java
+++ src/test/java/dev/alvartaco/notifications/service/CategoryServiceTests.java
@@ -10,6 +10,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +34,7 @@ public class CategoryServiceTests {
         Category mockCategory = new Category(testCategoryId,
                 testCategoryName);
 
+        // Mock the behavior of the repository to return the mock Category
         Mockito.when((jdbcClientCategoryRepository
                         .findByCategoryId(testCategoryId)))
                 .thenReturn(Optional.of(mockCategory));
@@ -54,6 +58,7 @@ public class CategoryServiceTests {
         Category mockCategory = new Category(testCategoryId,
                 testCategoryName);
 
+        // Mock the behavior of the repository to return the mock Category
         Mockito.when((jdbcClientCategoryRepository
                 .findByCategoryId(testCategoryId)))
                 .thenReturn(Optional.of(mockCategory));
@@ -70,14 +75,36 @@ public class CategoryServiceTests {
 
     @Test
     public void getAllCategoryDTOsByCategoryNameAsc() throws CategoryException {
-/*
-            List<CategoryDTO> categoriesDTO = new ArrayList<>();
-            for (Category category : iCategoryRepository.findAllByCategoryNameAsc()) {
-                categoriesDTO.add(mapToDTO(category));
-            }
-            log.info("#NOTIFICATIONS - OK getAllCategoryDTOsByCategoryNameAsc().");
-            return categoriesDTO;
-*/
+
+        Short testCategoryId = 1;
+        String testCategoryName = "Mock Category Name1";
+        Category mockCategory1 = new Category(testCategoryId,testCategoryName);
+        Short testCategoryId2 = 2;
+        String testCategoryName2 = "Mock Category Name2";
+        Category mockCategory2 = new Category(testCategoryId2,testCategoryName2);
+        List<Category> mockCategories = Arrays.asList(mockCategory1, mockCategory2);
+        List<CategoryDTO> mockCategoriesDTOs =
+                Arrays.asList(
+                        categoryService.mapToDTO(mockCategory1),
+                        categoryService.mapToDTO(mockCategory2));
+
+        // Mock the behavior of the repository to return the mock Categories
+        Mockito.when((jdbcClientCategoryRepository
+                        .findAllByCategoryNameAsc()))
+                    .thenReturn(mockCategories);
+
+        List<CategoryDTO> results =
+                categoryService.getAllCategoryDTOsByCategoryNameAsc();
+
+        //assertEquals(mockCategoriesDTOs.size(), results.size());
+        assertTrue(mockCategoriesDTOs.containsAll(results));
+        assertTrue(results.containsAll(mockCategoriesDTOs));
+        /*
+        assertEquals("Verify same metrics series",
+                CollectionUtils.getCardinalityMap(mockCategoriesDTOs),
+                CollectionUtils.getCardinalityMap(mockCategoriesDTOs));
+
+         */
     }
 
 }
